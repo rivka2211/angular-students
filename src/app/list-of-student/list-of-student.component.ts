3fd0214297d5b2f91e91bdb83ebc29c8bf22d11b
rivka2211 <r0548500974@gmail.com>
2024-11-13 00:45:58 +0200

    new student

--- src/app/list-of-student/list-of-student.component.ts
+++ src/app/list-of-student/list-of-student.component.ts
@@ -13,5 +13,6 @@ arrStudent:student[]=[new student(1,"rivki","belz 15",true,100),
                      new student(2,"rut"," hashomer 154",true,92),
                      new student(3,"racheli","akiva",false,97),
                      new student(4,"shifi","bb 100",true,100),
+                      new student(5,"racheli","pt",true,99)
 ]
 }
